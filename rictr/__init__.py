098a581436daf052f1fca1c91b5c1f1ab3b0626b
prashantpandeygit <prashantpandeyiuet@gmail.com>
2026-01-29 09:41:16 +0530

    update with dir

--- rictr/__init__.py
+++ rictr/__init__.py
@@ -1,2 +1,40 @@
-from .core.distiller import Distiller
-__all__ = ["Distiller"]
+from .core import Distiller, Trainer, TrainingState, StepOutput
+from .strategies import (
+    DistillationStrategy,
+    SoftTarget,
+    HiddenStateDistillation,
+    Composite,
+)
+from .alignment import LayerMap, FeatureExtractor, make_projector, get_submodule
+from .losses import kl_divergence, mse_loss, cosine_loss, attention_loss
+from .utils import freeze, unfreeze, get_device, auto_device
+
+__version__ = "1.0.0"
+
+__all__ = [
+    # Core
+    "Distiller",
+    "Trainer",
+    "TrainingState",
+    "StepOutput",
+    # Strategies
+    "DistillationStrategy",
+    "SoftTarget",
+    "HiddenStateDistillation",
+    "Composite",
+    # Alignment
+    "LayerMap",
+    "FeatureExtractor",
+    "make_projector",
+    "get_submodule",
+    # Losses
+    "kl_divergence",
+    "mse_loss",
+    "cosine_loss",
+    "attention_loss",
+    # Utils
+    "freeze",
+    "unfreeze",
+    "get_device",
+    "auto_device",
+]
